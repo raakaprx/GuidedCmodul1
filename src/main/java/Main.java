a27383e691844a53fec1bcea49b880eb93d3cabd
raakaprx <raka101004@gmail.com>
2023-10-08 11:08:23 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,4 @@
+public class Main {public static void main(String args[]) { Welcome W=new Welcome (); W.display();
+}
+//2211103048
+}
---
+++ src/main/java/Welcome.java
@@ -0,0 +1,4 @@
+public class Welcome {public void display() {
+    System.out.println("Selamat Datang di Praktikum PBO");
+}
+}
